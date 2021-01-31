266ff399ee4b7a5408f91363aa299a8bb9d45ac2
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-31 19:27:10 +0000

    deleted tester file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from './components/Header'
-import Tester from './components/Tester'
 
 function App() {
   return (
     <div className="container">
       <Header title="Sophia Tolani Solomon"/>
-      <Tester />
     </div>
   );
 }
